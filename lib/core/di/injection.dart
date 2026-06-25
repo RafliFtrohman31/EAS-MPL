0c44d966f85722b513358afe8cee6fe5c3bf8d41
PIK15 <opikjk9@gmail.com>
2026-06-25 18:53:33 +0700

    feat: register api service into get_it service locator

--- lib/core/di/injection.dart
+++ lib/core/di/injection.dart
@@ -1,17 +1,18 @@
 import 'package:get_it/get_it.dart';
 import 'package:dio/dio.dart';
 import '../config/env_config.dart';
+import '../network/api_service.dart'; // Import ApiService baru
 
-final locator = GetIt.instance;
+final locator = GetIt.instance; // Sesuai Modul 15
 
 void setupLocator() {
-
+  // 1. Register Dio (Network) - Sudah ada dari Modul 15
   locator.registerLazySingleton<Dio>(() {
     final dio = Dio(BaseOptions(baseUrl: EnvConfig.baseUrl));
-
     dio.interceptors.add(LogInterceptor(requestBody: true, responseBody: true));
     return dio;
   });
-  
 
+  // 2. Register ApiService baru
+  locator.registerLazySingleton<ApiService>(() => ApiService(locator<Dio>()));
 }
