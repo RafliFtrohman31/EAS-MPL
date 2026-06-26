8a90d4bef252900d27b19ed954eab38d80c8516e
PIK15 <opikjk9@gmail.com>
2026-06-26 17:18:43 +0700

    daftarkan ke router

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -3,6 +3,7 @@ import 'package:go_router/go_router.dart';
 import 'package:uas_mobile_lanjut/core/di/injection.dart';
 import 'package:uas_mobile_lanjut/features/home/presentation/cubit/news_cubit.dart';
 import 'package:uas_mobile_lanjut/features/home/presentation/pages/home_page.dart';
+import 'package:uas_mobile_lanjut/features/home/presentation/pages/news_detail_page.dart'; // 🛠️ Tambahkan impor ini
 import 'package:uas_mobile_lanjut/features/settings/presentation/pages/profile_page.dart';
 
 class AppRouter {
@@ -14,9 +15,18 @@ class AppRouter {
       GoRoute(
         path: '/',
         builder: (context, state) => BlocProvider(
-          create: (context) => locator<NewsCubit>()..loadNewsPortal(), // Menginisialisasi data berita secara otomatis
+          create: (context) => locator<NewsCubit>()..loadNewsPortal(),
           child: const HomePage(),
         ),
+      ),
+      // 🛠️ DAFTARKAN RUTE DETAIL DI SINI
+      GoRoute(
+        path: '/detail',
+        builder: (context, state) {
+          // Menangkap objek artikel yang dikirim lewat parameter 'extra' dari HomePage
+          final article = state.extra; 
+          return NewsDetailPage(article: article);
+        },
       ),
       GoRoute(
         path: '/profile',
