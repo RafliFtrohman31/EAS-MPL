8db7a79c0cdedbfecd0d8eaeadc189597abadb37
PIK15 <opikjk9@gmail.com>
2026-06-26 10:23:36 +0700

    feat: implement profile page with 8-tap Easter Egg logic for NPM 20123048

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,9 +1,9 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:uas_mobile_lanjut/core/di/injection.dart';
 import 'package:uas_mobile_lanjut/features/home/presentation/cubit/news_cubit.dart';
 import 'package:uas_mobile_lanjut/features/home/presentation/pages/home_page.dart';
+import 'package:uas_mobile_lanjut/features/settings/presentation/pages/profile_page.dart'; // Impor Halaman Profil
 
 class AppRouter {
   AppRouter._();
@@ -13,11 +13,15 @@ class AppRouter {
     routes: [
       GoRoute(
         path: '/',
-        // Menggunakan BlocProvider di level routing agar NewsCubit bisa diakses oleh HomePage
         builder: (context, state) => BlocProvider(
           create: (context) => locator<NewsCubit>(),
           child: const HomePage(),
         ),
+      ),
+      // Tambahkan route khusus ke halaman profil untuk pemanggilan navigasi UI
+      GoRoute(
+        path: '/profile',
+        builder: (context, state) => const ProfilePage(),
       ),
     ],
   );
